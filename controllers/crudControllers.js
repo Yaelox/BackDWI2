dad79cd7401dd2bfc44490b9a70cf730fe8f45ec
yael <yaelsilvestre64@gmail.com>
2024-08-03 16:09:05 -0600

    ARREGLO API ACT

--- controllers/crudControllers.js
+++ controllers/crudControllers.js
@@ -33,7 +33,6 @@ const deleteUser = (req, res) => {
         }
     });
 };
-
 const updateUser = (req, res) => {
     const userId = req.params.id;
     const { usuario, fecha_nacimiento, email, rol } = req.body;
@@ -44,36 +43,22 @@ const updateUser = (req, res) => {
         return res.status(400).json({ mensaje: 'El rol es requerido' });
     }
 
-    const getRoleIdQuery = 'SELECT id FROM roles WHERE role_name = ?';
-    db.query(getRoleIdQuery, [rol], (err, roleResult) => {
+    const query = 'UPDATE Usuarios SET usuario = ?, fecha_nacimiento = ?, email = ?, role_id = ? WHERE ID_Usuario = ?';
+    
+    // Obtener el ID del rol directamente en la consulta
+    const params = [usuario, fecha_nacimiento, email, rol, userId];
+
+    console.log('Consulta de actualización:', query);
+    console.log('Parámetros de actualización:', params);
+
+    db.query(query, params, (err, result) => {
         if (err) {
-            console.error('Error al obtener el ID del rol:', err);
-            return res.status(500).send('Error del servidor al obtener el ID del rol');
+            console.error('Error al actualizar usuario:', err);
+            return res.status(500).send('Error del servidor al actualizar usuario');
+        } else {
+            console.log('Resultado de la actualización:', result);
+            return res.json({ mensaje: 'Usuario actualizado exitosamente' });
         }
-
-        console.log('Resultado de la consulta del rol:', roleResult);
-
-        if (roleResult.length === 0) {
-            return res.status(400).json({ mensaje: 'Rol no válido' });
-        }
-
-        const roleId = roleResult[0].id;
-
-        const query = 'UPDATE Usuarios SET usuario = ?, fecha_nacimiento = ?, email = ?, role_id = ? WHERE ID_Usuario = ?';
-        const params = [usuario, fecha_nacimiento, email, roleId, userId];
-
-        console.log('Consulta de actualización:', query);
-        console.log('Parámetros de actualización:', params);
-
-        db.query(query, params, (err, result) => {
-            if (err) {
-                console.error('Error al actualizar usuario:', err);
-                return res.status(500).send('Error del servidor al actualizar usuario');
-            } else {
-                console.log('Resultado de la actualización:', result);
-                return res.json({ mensaje: 'Usuario actualizado exitosamente' });
-            }
-        });
     });
 };
 
